2985c7ce04a3af6fda06188bb608056c89cafeb8
Entrar <Entrar@APD-L13>
2017-09-22 13:06:22 +0100

    Kata1 version 2: calcular edad de una persona usando clase Calendar y GregorianCalendar

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,14 +1,18 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Kata1 {
 
     public static void main(String[] args) {
+        Calendar date = GregorianCalendar.getInstance();
+        date.set(1996, 04,25);
         
-        Person person = new Person("Michael", new Date(96,04,25));
+        Person person = new Person("Michael", date);
         System.out.println(person.getName() + " tiene " + 
                             person.getAge() + " años");
+            
     }
     
 }
